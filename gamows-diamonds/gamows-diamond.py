e8279edbfe3c5feaaf4131caa87f238f0303ddd1
David Noe <info@davidnoe.ch>
2022-11-02 11:49:18 +0100

    Additional comments

--- gamows-diamonds/gamows-diamond.py
+++ gamows-diamonds/gamows-diamond.py
@@ -1,4 +1,5 @@
 # Helper functions
+# Generate all possible codons based on the 4 bases
 def assemble_codons():
     bases = ['A', 'T', 'G', 'C']
     codons = []
@@ -9,6 +10,7 @@ def assemble_codons():
     return codons
 
 
+# Create complementary strand based on string input
 def create_complementary_strand(strand: str) -> str:
     mapping = [['A', 'T', 'G', 'C'], ['T', 'A', 'C', 'G']]
     complementary = ''
@@ -19,6 +21,7 @@ def create_complementary_strand(strand: str) -> str:
     return complementary
 
 
+# Generate a list of complementary stands
 def create_complementary_list(codons: list) -> list:
     lys = []
     for codon in codons:
@@ -26,10 +29,11 @@ def create_complementary_list(codons: list) -> list:
     return lys
 
 
+# Create a list of possible diamonds based on gamow's diamonds theory
 def create_possible_diamonds(input_codon: list) -> list:
     complement_codon = create_complementary_list(input_codon)
 
-    diamond_paths = [[0, 1, 3, 2], [0, 2, 3, 1], [3, 1, 0, 2], [3, 2, 0, 1]]
+    diamond_paths = [[0, 1, 3, 2], [0, 2, 3, 1], [3, 1, 0, 2], [3, 2, 0, 1]] # possible ways to read the diamond strand
     possible_diamonds = []
     for i in range(len(input_codon)):
         aa_tot = input_codon[i][0:2] + complement_codon[i][1::]
@@ -44,6 +48,7 @@ def create_possible_diamonds(input_codon: list) -> list:
     return possible_diamonds
 
 
+# Convert dictionary values into a list of values, remove duplicates
 def extract_unique_sub_list(dic: dict) -> list:
     lys = []
     for key in dic:
@@ -54,6 +59,7 @@ def extract_unique_sub_list(dic: dict) -> list:
     return lys
 
 
+# Create list of unique amino acids codons -> based on diamond-theory -> genetic code by gamow
 def create_final_list(input_codons):
     dic = {}
     for i in range(len(input_codons)):
@@ -70,7 +76,7 @@ def create_final_list(input_codons):
     return lys
 
 
-# Procedural
+# Procedural -> Get output required by problem
 codon_list = assemble_codons()
 final_list = create_final_list(codon_list)
 
